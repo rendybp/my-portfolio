48caeb78b3c306843d611d7f298943751b649812
rendybp <rendibuanap@gmail.com>
2025-10-10 00:52:12 +0700

    navbar already responsive for mobile

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,10 +1,26 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { useRef } from 'react';
 
 function Navbar() {
+
+    const sideMenuRef = useRef();
+    
+    const openMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+    
+    const closeMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
+
     return (
         <>
+        <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+            <Image src={assets.header_bg_color} alt='' className='w-full'/>
+        </div>
+
         <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
             <a href="#top">
                 <Image src={assets.logo} alt="" className='w-28 cursor-pointer mr-14'/>
@@ -18,9 +34,35 @@ function Navbar() {
                 <li><a className='font-ovo' href="#contact">Contact Me</a></li>
             </ul>
 
-            <div>
-                <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo'>Contact <Image src={assets.arrow_icon} alt="" className='w-3'/> </a>
+            <div className='flex items-center gap-4'>
+
+                <button>
+                    <Image src={assets.moon_icon} alt='' className='w-6'/>
+                </button>
+
+                <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo'>
+                    Contact <Image src={assets.arrow_icon} alt="" className='w-3'/> 
+                </a>
+
+                <button className='block md:hidden ml-3' onClick={openMenu}>
+                    <Image src={assets.menu_black} alt='' className='w-6'/>
+                </button>
             </div>
+
+            {/* Mobile Menu */}
+
+            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+                
+                <div className='absolute right-6 top-6' onClick={closeMenu}>
+                    <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+                </div>
+
+                <li><a className='font-ovo' onClick={closeMenu} href="#top">Home</a></li>
+                <li><a className='font-ovo' onClick={closeMenu} href="#about">About Me</a></li>
+                <li><a className='font-ovo' onClick={closeMenu} href="#services">Services</a></li>
+                <li><a className='font-ovo' onClick={closeMenu} href="#work">My Work</a></li>
+                <li><a className='font-ovo' onClick={closeMenu} href="#contact">Contact Me</a></li>
+            </ul>
         </nav>
         </>
     )
