693355d5008dfbee950ee18b160b877c871f6f8b
rendybp <rendibuanap@gmail.com>
2025-10-10 21:48:03 +0700

    add changes on about section

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,28 +4,30 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div id='about' className='w-full px-[12%] py-10 scroll-mt-20'>
-            <h4 className='text-center mb-2 text-lg font-ovo'>Introduction</h4>
-            <h2 className='text-center text-5xl font-ovo'>About me</h2>
+        <div id='about' className='w-full px-6 lg:px-8 py-10 scroll-mt-20'>
+            <div className='mx-auto max-w-6xl'>
+                <h4 className='text-center mb-2 text-lg font-ovo'>Introduction</h4>
+                <h2 className='text-center text-5xl font-ovo'>About me</h2>
 
-            <div className='flex w-full flex-col lg:flex-row items-center gap-20 my-20'>
-                <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
-                    <Image src={assets.user_image} alt='user' className='w-full rounded-3xl'/>
-                </div>
-                <div className='flex-1'>
-                    <p className='mb-10 max-w-2xl font-ovo'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sapiente sunt dignissimos quibusdam laborum, dolorum ab repudiandae excepturi! Pariatur omnis magnam adipisci natus labore alias ipsa consequatur repellat optio accusantium?
-                    </p>
+                <div className='flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 my-20'>
+                    <div className='w-64 sm:w-80 rounded-3xl'>
+                        <Image src={assets.user_image} alt='user' className='w-full rounded-3xl'/>
+                    </div>
+                    <div className='flex-1 max-w-2xl'>
+                        <p className='mb-10 font-ovo'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui minus dolores beatae asperiores ipsa nemo esse totam, sit reiciendis sapiente cumque sint ea fuga eligendi nihil vel! Fugit, deserunt corrupti facere ipsa expedita veniam est vero id aspernatur, rerum quis aut incidunt sint labore quae ab similique nisi illo at obcaecati qui vitae reiciendis itaque? Atque, ullam totam beatae aspernatur odio pariatur aliquam voluptatibus quos reiciendis, suscipit eos ea voluptatum distinctio sequi adipisci earum autem rerum accusamus ex cupiditate, sunt eum dolorum nulla ipsa. Sit nisi velit dolorum quasi officiis, odio ipsam voluptas placeat iste doloribus, culpa provident porro ab.
+                        </p>
 
-                    <ul>
-                        {infoList.map(({icon, iconDark, title, description}, index) => (
-                            <li key={index}>
-                                <Image src={icon} alt='title'/>
-                                <h3>{title}</h3>
-                                <p>{description}</p>
-                            </li>
-                        ))}
-                    </ul>
+                        <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6'>
+                            {infoList.map(({icon, iconDark, title, description}, index) => (
+                                <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer' key={index}>
+                                    <Image src={icon} alt={title} className='w-7 mt-3'/>
+                                    <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
+                                    <p className='text-gray-600 text-sm'>{description}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
