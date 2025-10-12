072aa94c914b1cd68add7e6887e1235f4698a5a4
rendybp <rendibuanap@gmail.com>
2025-10-13 00:30:37 +0700

    add footer with little fix on font settings

--- app/layout.js
+++ app/layout.js
@@ -3,12 +3,14 @@ import "./globals.css";
 
 const outfit = Outfit({
   subsets: ["latin"],
-  weight : ["400", "500", "600", "700"]
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-outfit",
 });
 
 const ovo = Ovo({
   subsets: ["latin"],
-  weight : ["400"]
+  weight: ["400"],
+  variable: "--font-ovo",
 });
 
 export const metadata = {
@@ -20,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className='scroll-smooth'>
       <body
-        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
+        className={`${outfit.variable} ${ovo.variable} font-outfit antialiased leading-8 overflow-x-hidden`}
       >
         {children}
       </body>
