b662fb46eb7e01d8b1cbbd93e052d2534c7755c5
rendybp <rendibuanap@gmail.com>
2025-10-15 22:41:15 +0700

    fixing the dark theme color issue in services and work section

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -2,7 +2,7 @@ import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Work = () => {
+const Work = (isDarkMode) => {
     return (
         <div id='work' className='w-full px-6 lg:px-8 py-10 scroll-mt-20'>
             <div className='mx-auto max-w-7xl'>
@@ -13,7 +13,7 @@ const Work = () => {
                     Welcome to my web development portfolio ! Explore a collection of projects showcasing my expertise in web developer
                 </p>
 
-                <div className='grid gap-5 my-10 grid-cols-[repeat(auto-fit,minmax(220px,1fr))]'>
+                <div className='grid gap-5 my-10 grid-cols-[repeat(auto-fit,minmax(220px,1fr))] dark:text-black'>
                     {workData.map((project,index)=>(
                         <div key={index}
                         className='aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group overflow-hidden'
@@ -30,8 +30,8 @@ const Work = () => {
                         </div>
                     ))}
                 </div>
-                <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full px-10 py-3 mx-auto my-20 hover:bg-lightHover duration-500'>
-                    Show more <Image src={assets.right_arrow_bold} alt='Right Arrow' className='w-4'/>
+                <a href="" className='w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full px-10 py-3 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover'>
+                    Show more <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='Right Arrow' className='w-4'/>
                 </a>
             </div>
         </div>
