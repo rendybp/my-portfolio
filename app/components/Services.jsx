a05b090f5086f18a102fbd1c0252d21778bbaafa
rendybp <rendibuanap@gmail.com>
2025-10-15 23:28:01 +0700

    add animaiton in services section

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,21 +1,44 @@
 import { assets, serviceData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Services = () => {
     return (
-        <div id='services' className='w-full px-6 lg:px-8 py-10 scroll-mt-20'>
+        <motion.div 
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 1}}
+        id='services' className='w-full px-6 lg:px-8 py-10 scroll-mt-20'>
             <div className='mx-auto max-w-7xl'>
-                <h4 className='text-center mb-2 text-lg font-ovo'>What I Offer</h4>
-                <h2 className='text-center text-5xl font-ovo'>My Services</h2>
+                <motion.h4 
+                initial={{y: -20, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{delay: 0.3, duration: 0.5}}
+                className='text-center mb-2 text-lg font-ovo'>What I Offer</motion.h4>
+                <motion.h2 
+                initial={{y: -20, opacity: 0}}
+                whileInView={{y: 0, opacity: 1}}
+                transition={{delay: 0.5, duration: 0.5}}
+                className='text-center text-5xl font-ovo'>My Services</motion.h2>
 
-                <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>
+                <motion.p 
+                initial={{opacity: 0}}
+                whileInView={{opacity: 1}}
+                transition={{delay: 0.7, duration: 0.5}}
+                className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>
                     I am a web developer from Indonesia with 5 years of experience in various programming language such as Javascript, PHP, React, Laravel, etc.
-                </p>
+                </motion.p>
 
-                <div className='grid gap-6 my-10 grid-cols-[repeat(auto-fit,minmax(220px,1fr))]'>
+                <motion.div 
+                initial={{opacity: 0}}
+                whileInView={{opacity: 1}}
+                transition={{delay: 0.9, duration: 0.6}}
+                className='grid gap-6 my-10 grid-cols-[repeat(auto-fit,minmax(220px,1fr))]'>
                     {serviceData.map(({icon, title, description, link}, index)=>(
-                        <div key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'>
+                        <motion.div 
+                        whileHover={{scale: 1.05}}
+                        key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 dark:hover:bg-darkHover dark:hover:shadow-white'>
                             <Image src={icon} alt='' className='w-10'/>
                             <h3 className='text-lg my-4 text-gray-700 dark:text-white'>{title}</h3>
                             <p className='text-sm text-gray-600 leading-5 dark:text-white/80'>
@@ -24,11 +47,11 @@ const Services = () => {
                             <a href={link} className='flex items-center gap-2 text-sm mt-5'>
                                 Read more <Image src={assets.right_arrow} alt='' className='w-4'/>
                             </a>
-                        </div>
+                        </motion.div>
                     ))}
-                </div>
+                </motion.div>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
