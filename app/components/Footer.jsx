f1277281cd07f30e3aeda3d4e9e1b427aa86d019
rendybp <rendibuanap@gmail.com>
2025-11-06 15:08:40 +0700

    fixing logo scroll problem and better email message

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -6,13 +6,12 @@ const Footer = ({isDarkMode}) => {
     return (
         <div className='mt-20'>
             <div className='text-center'>
-                <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2'/>
-
+                <a href="#top">
+                    <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+                </a>
                 <div className='w-max flex items-center gap-2 mx-auto'>
-                    <a href="#top">
-                        <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
-                    </a>
-                    <a href="mailto:contact.rendibp@gmail.com">contact.rendibp@gmail.com</a>
+                    <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
+                    <a href="mailto:contact.rendibp@gmail.com?subject=Job%20Offer%20or%20Partnership&body=Hi,%20I%20have%20a%20job%20offer%20and%20a%20partnership%20opportunity%20for%20you.">contact.rendibp@gmail.com</a>
                 </div>
             </div>
             <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
